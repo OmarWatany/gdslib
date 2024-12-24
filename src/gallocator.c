6feca33f0caa20d3b9c89d76ceee059a18c3a97a
Omar Watany <watany6500@gmail.com>
2024-12-24 23:59:49 +0200

    custom deallocator

--- src/gallocator.c
+++ src/gallocator.c
@@ -1,6 +1,12 @@
 #include "gallocator.h"
 #include <string.h>
 
+Gallocator_t gallocator = (Gallocator_t){
+    .deallocator = default_deallocator,
+};
+
+Gallocator_t *default_gallocator = &gallocator;
+
 gdata_t default_allocator(size_t item_size, gdata_t data) {
     return default_safe_allocator(item_size, item_size, data);
 }
@@ -13,4 +19,10 @@ gdata_t default_safe_allocator(size_t allocation_size, size_t item_size, gdata_t
 
 gdata_t str_allocator(gdata_t str) {
     return strdup((char *)str);
+}
+
+int default_deallocator(gdata_t data) {
+    if (!data) return EXIT_FAILURE;
+    free(data);
+    return EXIT_SUCCESS;
 }
